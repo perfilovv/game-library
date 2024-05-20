1dbada3c9e7894b90e5bab15a1dbb91035ce15f7
perfilovv <russianspiritoneshotlol@gmail.com>
2024-05-20 14:04:21 +0300

    feat: add types for GameCard component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-const GameCard = () => {
+interface Game {
+  id: number;
+  name: string;
+  rating: number;
+  platforms: string[];
+  maxPlayersOnline: number;
+  maxPlayersOffline: number;
+  languages: string[];
+}
+
+interface GameCardProps {
+  game: Game;
+}
+
+const GameCard: FC<GameCardProps> = () => {
   return (
     <div className='bg-gray-100 p-4 rounded-lg shadow-md'>
       <h2 className='text-xl font-semibold mb-2'>GameName</h2>
