0f5562d6aafd59cda9503357c99f70eba0bb74a0
perfilovv <russianspiritoneshotlol@gmail.com>
2024-05-20 12:20:55 +0300

    feat: add right structure for project

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GameCards = () => {
+  return <div>GameCards</div>;
+};
+
+export default GameCards;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import GameCards from '@/components/GameCard';
+import React from 'react';
+
+const index = () => {
+  return (
+    <>
+      <GameCards />
+    </>
+  );
+};
+
+export default index;
