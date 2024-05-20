9d85a8c880f7303818eebc88277d1ad4facb483e
perfilovv <russianspiritoneshotlol@gmail.com>
2024-05-20 17:02:04 +0300

    feat: add static game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -16,13 +16,49 @@ interface GameCardProps {
 
 const GameCard: FC<GameCardProps> = () => {
   return (
-    <div className='bg-gray-100 p-4 rounded-lg shadow-md'>
-      <h2 className='text-xl font-semibold mb-2'>GameName</h2>
-      <p>Rating:</p>
-      <p>Platforms:</p>
-      <p>Max Players Online:</p>
-      <p>Max Players Offline:</p>
-      <p>Languages:</p>
+    <div className='container mx-auto p-4'>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+        <div className='bg-gray-100 p-4 rounded-lg shadow-md'>
+          <h2 className='text-xl font-semibold mb-2'>GameName</h2>
+          <p>Rating:</p>
+          <p>Platforms:</p>
+          <p>Max Players Online:</p>
+          <p>Max Players Offline:</p>
+          <p>Languages:</p>
+        </div>
+        <div className='bg-gray-100 p-4 rounded-lg shadow-md'>
+          <h2 className='text-xl font-semibold mb-2'>GameName</h2>
+          <p>Rating:</p>
+          <p>Platforms:</p>
+          <p>Max Players Online:</p>
+          <p>Max Players Offline:</p>
+          <p>Languages:</p>
+        </div>
+        <div className='bg-gray-100 p-4 rounded-lg shadow-md'>
+          <h2 className='text-xl font-semibold mb-2'>GameName</h2>
+          <p>Rating:</p>
+          <p>Platforms:</p>
+          <p>Max Players Online:</p>
+          <p>Max Players Offline:</p>
+          <p>Languages:</p>
+        </div>
+        <div className='bg-gray-100 p-4 rounded-lg shadow-md'>
+          <h2 className='text-xl font-semibold mb-2'>GameName</h2>
+          <p>Rating:</p>
+          <p>Platforms:</p>
+          <p>Max Players Online:</p>
+          <p>Max Players Offline:</p>
+          <p>Languages:</p>
+        </div>
+        <div className='bg-gray-100 p-4 rounded-lg shadow-md'>
+          <h2 className='text-xl font-semibold mb-2'>GameName</h2>
+          <p>Rating:</p>
+          <p>Platforms:</p>
+          <p>Max Players Online:</p>
+          <p>Max Players Offline:</p>
+          <p>Languages:</p>
+        </div>
+      </div>
     </div>
   );
 };
